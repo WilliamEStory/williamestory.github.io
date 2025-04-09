d50deb639869f5eaa9a98cb19e770bffcfff3260
William Story <william.story.e@gmail.com>
2025-04-09 12:14:25 -0600

    Add themeing, photo of me, name, job title

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import { Button } from "@/components/ui/button"
+import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar"
+import MyAvatar from "@/assets/avatar-will.jpg"
+import { ThemeProvider } from "./providers/theme-provider"
 
 function App() {
 
   return (
-    <div className="place-items-center mt-20">
-      <h1>Under construction 🔨</h1>
-      <Button onClick={() => alert("hi")}>Click me for a cool message</Button>
-    </div>
+    <ThemeProvider defaultTheme="dark">
+      <div className="place-items-center mt-20 gap-2 flex flex-col">
+        <img src={MyAvatar} alt="My Avatar" className="rounded-full w-32 h-32 object-cover" />
+        <h1 className="text-4xl font-bold">William Story</h1>
+        <h2 className="text-3xl font-bold">Software Engineer</h2>
+      </div>
+    </ThemeProvider>
   )
 }
 
